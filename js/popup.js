0afa68af50873e1bfb5e60f1e123adbbf0a36644
Ketan Mujumdar <ketanmujumdar@gmail.com>
2013-06-21 00:27:01 +0800

    https://bitbucket.org/ketanmujumdar/gaana-extention/issue/7/add-ga-tracking-to-extention
    https://bitbucket.org/ketanmujumdar/gaana-extention/issue/10/extension-used-to-crash-when-no-data-in
    
    Fixed above two issues

--- js/popup.js
+++ js/popup.js
@@ -1,30 +1,8 @@
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-
-// function click(e) {
-//   chrome.tabs.executeScript(null,
-//       {code:"document.body.style.backgroundColor='" + e.target.id + "'"});
-//   window.close();
-// }
-
-// document.addEventListener('DOMContentLoaded', function () {
-//   var divs = document.querySelectorAll('div');
-//   for (var i = 0; i < divs.length; i++) {
-//     divs[i].addEventListener('click', click);
-//   }
-// });
-
-
-
 jQuery(document).ready(function(){
 
 var tab_id = 0;
 	
-setSongInfo();
-//auto refresh popup every 5 second, if its opened for long
-setTimeout(function(){window.location.href = ext_popup;}, 5000);
+
 
 	chrome.tabs.query({'url': '*://'+ext_site_name+'/*'},function(tab){
 		try
@@ -39,6 +17,9 @@ setTimeout(function(){window.location.href = ext_popup;}, 5000);
 		}
     });
 
+	setSongInfo();
+	//auto refresh popup every 5 second, if its opened for long
+	setTimeout(function(){window.location.href = ext_popup;}, 5000);
 
 	jQuery('.extPlayPause').click(function(){
 	chrome.tabs.executeScript(tab_id, { file: "js/jquery.js" }, function() {
@@ -83,13 +64,36 @@ setTimeout(function(){window.location.href = ext_popup;}, 5000);
 		// jQuery('.extPlayPause i').addClass('icon-pause');
 	});
 
-
 	function setSongInfo() {
-		var SongInfo = JSON.parse(get(ext_name+'SongInfo'));
-		jQuery('#extImage').attr('src' , SongInfo.ico);
-		jQuery('#extName').html(SongInfo.title);
-		jQuery('#extAlbum').html(SongInfo.album);
+		try {
+			var SongInfo = JSON.parse(get(ext_name+'SongInfo'));
+			if(SongInfo == undefined || SongInfo == 'undefined' || SongInfo == null)
+			{
+				jQuery('#wrapper').hide();
+				jQuery('#warning').show();
+			}
+			else {
+				jQuery('#wrapper').show();
+				jQuery('#warning').hide();
+				jQuery('#extImage').attr('src' , SongInfo.ico);
+				jQuery('#extName').html(SongInfo.title);
+				jQuery('#extAlbum').html(SongInfo.album);	
+			}
+		}
+		catch(exp) {
+			
+		}
 	}
-
-
 });
+
+	//GA Tracking
+
+      var _gaq = _gaq || [];
+     _gaq.push(['_setAccount', 'UA-41905417-1']);
+          _gaq.push(['_trackPageview']);
+
+    (function() {
+      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+      ga.src = 'https://ssl.google-analytics.com/ga.js';
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
